870a4433f434438bd142ab1509fe2a299573baba
OliveMyki49 <cmbarcellano@forbescollege.org>
2023-02-20 11:58:05 +0800

    Formatting using tailwind

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -2,19 +2,71 @@
 
 @section('content') {{-- Display this in content section of layout blade php file --}}
 
-<h2><?php echo $heading ?></h2> <!-- pass the value from route -->
+@include('partials._hero') {{-- Partial folder like component --}}
+@include('partials._search') {{-- Partial folder like component --}}
 
-@if(count($listings) == 0){{-- Display no data fond if listings array is empty --}}
-    <p>No Data Found</p>
-@endif
+<div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+     <!-- pass the value from route -->
+    {{-- <h2><?php //echo $heading ?></h2> --}}
 
-@foreach($listings as $item)<!-- pass data using foreach -->
-        <h3>
-            <a href="/listing/{{$item['id']}}"> {{$item['id']}} | {{$item['title']}}</a>
-        </h3>
-        <p>{{$item['description']}}</p>
-    </tr>
-</table>
-@endforeach;
+    @unless(count($listings) == 0){{-- Display no data fond if listings array is empty --}}
+        
 
+    @foreach($listings as $item)<!-- pass data using foreach -->
+            {{-- THIS IS OLD FORMAT --}}
+            {{-- 
+                <h3>
+                    <a href="/listing/{{$item['id']}}"> {{$item['id']}} | {{$item['title']}}</a>
+                </h3>
+                <p>{{$item['description']}}</p> 
+            --}}
+
+            
+            <div class="bg-gray-50 border border-gray-200 rounded p-6">
+                <div class="flex">
+                    <img
+                        class="hidden w-48 mr-6 md:block"
+                        src="{{ asset('images/no-image.png') }}"
+                        alt=""
+                    />
+                    <div>
+                        <h3 class="text-2xl">
+                            <a href="/listing/{{ $item->id }}">{{ $item->id }} | {{ $item->title }}</a>
+                        </h3>
+                        <div class="text-xl font-bold mb-4">{{ $item->company }}</div>
+                        <ul class="flex">
+                            <li
+                                class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                            >
+                                <a href="#">Laravel</a>
+                            </li>
+                            <li
+                                class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                            >
+                                <a href="#">API</a>
+                            </li>
+                            <li
+                                class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                            >
+                                <a href="#">Backend</a>
+                            </li>
+                            <li
+                                class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                            >
+                                <a href="#">Vue</a>
+                            </li>
+                        </ul>
+                        <div class="text-lg mt-4">
+                            <i class="fa-solid fa-location-dot"></i> {{ $item->location }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+    @endforeach;
+
+    @else
+        <p>No Data Found</p>
+    @endunless
+
+</div>
 @endsection {{-- end the layout content section here --}}
