9638bfb4cd58c058ab72fc228e859714ade9cea0
OliveMyki49 <cmbarcellano@forbescollege.org>
2023-02-20 16:16:22 +0800

    add value state get

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -19,7 +19,8 @@
                     type="text"
                     class="border border-gray-200 rounded p-2 w-full"
                     name="company"
-                />
+                    value="{{old('company')}}"
+                /> {{-- old('company') values stays even after error in form --}}
 
                 @error('company') {{-- show error message here if company input have error --}}
                     <p class="text-red-500 text-xs mt-1">
@@ -37,7 +38,9 @@
                     class="border border-gray-200 rounded p-2 w-full"
                     name="title"
                     placeholder="Example: Senior Laravel Developer"
-                />
+                    value="{{old('title')}}"
+                /> {{-- old('company') values stays even after error in form --}}
+
 
                 @error('title') {{-- show error message here if company input have error --}}
                 <p class="text-red-500 text-xs mt-1">
@@ -57,7 +60,8 @@
                     class="border border-gray-200 rounded p-2 w-full"
                     name="location"
                     placeholder="Example: Remote, Boston MA, etc"
-                />
+                    value="{{old('location')}}"
+                /> {{-- old('company') values stays even after error in form --}}
 
                 @error('location') {{-- show error message here if company input have error --}}
                 <p class="text-red-500 text-xs mt-1">
@@ -74,7 +78,9 @@
                     type="text"
                     class="border border-gray-200 rounded p-2 w-full"
                     name="email"
-                />
+                    value="{{old('email')}}"
+                /> {{-- old('company') values stays even after error in form --}}
+
                 @error('email') {{-- show error message here if company input have error --}}
                 <p class="text-red-500 text-xs mt-1">
                     {{$message}}
@@ -93,7 +99,9 @@
                     type="text"
                     class="border border-gray-200 rounded p-2 w-full"
                     name="website"
-                />
+                    value="{{old('company')}}"
+                /> {{-- old('company') values stays even after error in form --}}
+
                 @error('website') {{-- show error message here if company input have error --}}
                 <p class="text-red-500 text-xs mt-1">
                     {{$message}}
@@ -110,7 +118,9 @@
                     class="border border-gray-200 rounded p-2 w-full"
                     name="tags"
                     placeholder="Example: Laravel, Backend, Postgres, etc"
-                />
+                    value="{{old('company')}}"
+                /> {{-- old('company') values stays even after error in form --}}
+
                 @error('tags') {{-- show error message here if company input have error --}}
                 <p class="text-red-500 text-xs mt-1">
                     {{$message}}
@@ -141,7 +151,9 @@
                     name="description"
                     rows="10"
                     placeholder="Include tasks, requirements, salary, etc"
+                    value="{{old('company')}}"
                 ></textarea>
+                
                 @error('description') {{-- show error message here if company input have error --}}
                 <p class="text-red-500 text-xs mt-1">
                     {{$message}}
